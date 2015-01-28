15d4fea79b964e9af222bbe431cffd4c09196e6e
Rahul Gupta <rahul.gupta@yale.edu>
2015-01-28 02:56:34 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! This is a lot of fun!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('gleaming boar');
 });
 
 app.listen(process.env.PORT || 4000);
